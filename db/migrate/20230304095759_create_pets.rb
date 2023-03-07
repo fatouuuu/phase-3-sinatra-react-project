9ff5e9f4e4628a8d8452763d8ebca62d1a4a1112
fatouuuu <fatmaabdullrahim@gmail.com>
2023-03-07 05:06:34 +0300

    migrations for prod env

--- db/migrate/20230304095759_create_pets.rb
+++ db/migrate/20230304095759_create_pets.rb
@@ -1,4 +1,16 @@
 class CreatePets < ActiveRecord::Migration[6.1]
   def change
+    create_table :pets do |t|
+      t.string :breed_name
+      t.string :breed
+      t.string :color
+      t.string :breed_for
+      t.string :sex
+      t.string :lifespan
+      t.string :temperament
+      t.string :image_url
+      t.integer :user_id
+      t.timestamps
+    end
   end
 end
